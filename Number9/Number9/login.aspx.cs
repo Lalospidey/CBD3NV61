e38247ae916dc9b9ebc26c2d0fb9f90dd650d41b
Lalospidey <gerardo.vazquez7@gmail.com>
2016-05-29 02:21:25 -0500

    fase1-2

--- Number9/Number9/login.aspx.cs
+++ Number9/Number9/login.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Web.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
 namespace Number9
 {
     public partial class login : System.Web.UI.Page
     {
+       
+        private string strcon = WebConfigurationManager.ConnectionStrings["Numero9ConnectionString"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -16,6 +21,17 @@ namespace Number9
 
         protected void Button2_Click(object sender, EventArgs e)
         {
+            string Query = "select * from login where usuario ='" + this.TextBox1.Text + "' and pass='" + this.TextBox2.Text + "'";
+            SqlConnection con = new SqlConnection(strcon);
+            SqlCommand cmd = new SqlCommand(Query);
+            cmd.Connection = con;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            SqlDataReader dr = cmd.ExecuteReader();
+            
+            
+            con.Close();
+            
             Response.Write("<script type='text/javascript'> window.open('Cliente.aspx','_self'); </script>");
             Response.Write("<script type='text/javascript'> window.open('Administrador.aspx','_self'); </script>");
  
